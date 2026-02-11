058e1d7379e52e492ef921a78068d097c7e71d03
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-02-11 15:39:42 -0500

    Set up mongodb database

---
+++ pages/api/weather/zipcode/[zipcode].ts
@@ -0,0 +1,10 @@
+import dbConnect from "@/middleware/db-connect";
+import { findByZip } from "@/mongoose/weather/services";
+import { NextApiHandler, NextApiResponse } from "next";
+
+dbConnect();
+
+const handler: NextApiHandler = async (req, res): Promise<NextApiResponse<WeatherDetails> | void> => {
+	const data = await findByZip(req.query.zipcode as string);
+	return res.status(200).json(data);
+};
