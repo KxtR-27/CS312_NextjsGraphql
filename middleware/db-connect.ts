5fa1c5363ef4fb63744be2998692318cbaf0b6a6
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-02-13 13:13:16 -0500

    Add spying tests for middleware

--- middleware/db-connect.ts
+++ middleware/db-connect.ts
@@ -1,4 +1,4 @@
-import { storeDocument } from "@/mongoose/weather/services";
+import { storeDocument } from "../mongoose/weather/services";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 
@@ -7,29 +7,31 @@ const dbConnect = async (): Promise<any | string> => {
 	const MONGOIO_URI = mongoServer.getUri();
 	await mongoose.disconnect();
 
-	const db = await mongoose.connect(MONGOIO_URI, { dbName: "Weather" });
+	await mongoose.connect(MONGOIO_URI, { dbName: "Weather" });
 
-    await storeDocument({
-        zip: "96815",
-        weather: "sunny",
-        tempC: "20C",
-        tempF: "70F",
-        friends: ["96814", "96826"]
-    }),
-    await storeDocument({
-        zip: "96826",
-        weather: "sunny",
-        tempC: "25C",
-        tempF: "86F",
-        friends: ["96815", "96814"]
-    }),
-    await storeDocument({
-        zip: "96814",
-        weather: "sunny",
-        tempC: "20C",
-        tempF: "68F",
-        friends: ["96815", "96826"]
-    })
+	(await storeDocument({
+		zip: "96815",
+		weather: "sunny",
+		tempC: "20C",
+		tempF: "70F",
+		friends: ["96814", "96826"],
+	}),
+		await storeDocument({
+			zip: "96826",
+			weather: "sunny",
+			tempC: "25C",
+			tempF: "86F",
+			friends: ["96815", "96814"],
+		}),
+		await storeDocument({
+			zip: "96814",
+			weather: "sunny",
+			tempC: "20C",
+			tempF: "68F",
+			friends: ["96815", "96826"],
+		}));
+
+	return mongoServer;
 };
 
 export default dbConnect;
