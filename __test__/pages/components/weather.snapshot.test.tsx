ddcdd99c5cb4a116251d4a776ca54a59e0bad257
Kat R. <153114658+KxtR-27@users.noreply.github.com>
2026-02-13 14:29:24 -0500

    Implement second version of Weather component test
    looks like `react-test-renderer` is completely deprecated. thank you, lecture and activity PDFs that I had to fix several errors from thus far, incredibly suave.

--- __test__/pages/components/weather.snapshot.test.tsx
+++ __test__/pages/components/weather.snapshot.test.tsx
@@ -15,4 +15,15 @@ describe("PageComponentWeather", () => {
 
         expect(component.toJSON()).toMatchSnapshot()
     })
+
+    test("clicks the h1 element and updates the state", async() => {
+        let component: any;
+
+        await act(async () => {
+            component = create(<PageComponentWeather></PageComponentWeather>)
+            component.root.findByType("h1").props.onClick();
+        })
+
+        expect(component.toJSON()).toMatchSnapshot();
+    })
 })
